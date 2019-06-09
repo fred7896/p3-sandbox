9e925b4a814d3707152a3cfaa5ec24870fea5439
FredericMentel <frederic.mentel@gmail.com>
2019-06-09 19:06:46 +0200

    Add markers from ponds list.

--- frontend/src/components/MyMap.jsx
+++ frontend/src/components/MyMap.jsx
@@ -1,35 +1,42 @@
 import React from "react";
-import { Map, TileLayer, Marker } from "react-leaflet";
-import { connect } from 'react-redux';
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { connect } from "react-redux";
 
 class MyMap extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        zoomLevel: 16
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      zoomLevel: 12
+    };
   }
 
   render() {
     const center = this.props.center;
-      return (
-          <React.Fragment>
-  <Map center={center} zoom={this.state.zoomLevel}>
-    <TileLayer
-      url="http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png"
-      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-    />
-     <Marker position={center}>
-      {/* <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup> */}
-    </Marker> 
-  </Map>
-          </React.Fragment>
-      )
+    return (
+      <React.Fragment>
+        <Map center={center} zoom={this.state.zoomLevel}>
+          <TileLayer
+            url="http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+
+          {this.props.ponds.map((pond, idx) => {
+            return (
+              <React.Fragment key={idx}>
+                <Marker position={[pond.latitude, pond.longitude]}>
+                  <Popup>{pond.name}</Popup>
+                </Marker>
+              </React.Fragment>
+            );
+          })}
+        </Map>
+      </React.Fragment>
+    );
   }
 }
 
 const mapStateToProps = state => ({
-    center : state
+  center: state
 });
 
 const MapContainer = connect(mapStateToProps)(MyMap);
